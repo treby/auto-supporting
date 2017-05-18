c6570faebeb5af7479ad06c0cfc7f54df607337c
Hiroaki Ninomiya <me@treby.info>
2017-05-18 19:31:03 +0900

    use background page.

--- js/background.js
+++ js/background.js
@@ -1,1 +1,22 @@
-hello
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  const urls = msg.urls;
+  chrome.windows.create({ focused: false }, function(win) {
+    var winId = win.id;
+    var tabIds = [];
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+      if (!tabIds.includes(tabId)) return;
+      if (changeInfo.status && changeInfo.status === 'complete') {
+        chrome.tabs.sendMessage(tabId, {text: ''}, function(response) {
+          setTimeout(function() {
+            chrome.tabs.remove(tabId);
+          }, 8000);
+        });
+      }
+    });
+    urls.forEach(function(url) {
+      chrome.tabs.create({ windowId: winId, url: url }, function(tab) {
+        tabIds.push(tab.id);
+      });
+    });
+  });
+});
--- js/popup.js
+++ js/popup.js
@@ -2,28 +2,6 @@ var list = document.getElementById('list');
 var supportButton = document.getElementById('support');
 var convertButton = document.getElementById('convert');
 
-var travelWantedly = function(urls) {
-  chrome.windows.create({ active: false }, function(win) {
-    var winId = win.id;
-    var tabIds = [];
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-      if (!tabIds.includes(tabId)) return;
-      if (changeInfo.status && changeInfo.status === 'complete') {
-        chrome.tabs.sendMessage(tabId, {text: ''}, function(response) {
-          setTimeout(function() {
-            chrome.tabs.remove(tabId);
-          }, 8000);
-        });
-      }
-    });
-    urls.forEach(function(url) {
-      chrome.tabs.create({ windowId: winId, url: url }, function(tab) {
-        tabIds.push(tab.id);
-      });
-    });
-  });
-};
-
 var textToUrls = function(text) {
   return text.match(/https?:\/\/[\w/:%#\$&\?\(\)~\.=\+\-]+/g);
 };
@@ -31,8 +9,7 @@ var textToUrls = function(text) {
 supportButton.addEventListener('click', function(e) {
   e.preventDefault();
   var rawText = list.value;
-
-  travelWantedly(textToUrls(rawText));
+  chrome.runtime.sendMessage({ urls: textToUrls(rawText) });
 });
 convertButton.addEventListener('click', function(e) {
   e.preventDefault();
